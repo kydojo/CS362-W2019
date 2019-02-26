0a4158cf24be17390bc788be7795929053e52fb0
kydojo <kyledjohnson@hotmail.com>
2019-02-26 11:04:25 -0800

    Started work on smithy

--- dominion/randomtestcard1.c
+++ dominion/randomtestcard1.c
@@ -16,35 +16,43 @@
 
 // Set TESTING to 0 to remove print statements from console output
 #define TESTING 1
+#define NUM_TESTS 10000
+
+// Helper function to return a random number with the range of the two arguments
+int myRand( int low, int high ) {
+	return ( rand() % (high - low + 1) + low );
+}
 
 int main() {
 	
 	int seed = 1000;
-	int numPlayer = 2;
+	int numPlayers = 2;
 	int r, i;
 	int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
 	struct gameState G;
 
-	if( TESTING ) { printf( "TESTING CARD: smithy\n" ); }
+	//if( TESTING ) { printf( "TESTING CARD: smithy\n" ); }
 
-	initializeGame( numPlayer, k, seed, &G );
+	for( i = 0; i < NUM_TESTS; i++ ) {
 
-	// Test that hand count increases by 3 when smithy is played
+		initializeGame( numPlayers, k, seed, &G );
+
+		// Test that hand count increases by 3 when smithy is played
 	
-	// Set player's turn to player 1
-	G.whoseTurn = 0;
+		// Set player's turn to player 1
+		//G.whoseTurn = 0;
 
-	// Set the player's hand size to 1 card
-	G.handCount[0] = 1;
+		// Set the player's hand size to 1 card
+		G.handCount[0] = 1;
 
-	// Call cardEffect on smithy
-	cardEffect( smithy, 0, 0, 0, &G, 4, 0 );
+		// Call cardEffect on smithy
+		cardEffect( smithy, 0, 0, 0, &G, 4, 0 );
 
-	//printf( "Hand Size: %d\n", numHandCards( &G ) );
-	if( TESTING ) { 
-		printf( "\t\t+3 Cards" );
-		if( numHandCards( &G ) == 3 ) {
-			printf("\t\t\tPASS\n");
-		}
-		else {
-			printf("\t\t\tFAIL\n");
+		//printf( "Hand Size: %d\n", numHandCards( &G ) );
+		if( TESTING ) { 
+			printf( "\t\t+3 Cards" );
+			if( numHandCards( &G ) == 3 ) {
+				printf("\t\t\tPASS\n");
+			}
+			else {
+				printf("\t\t\tFAIL\n");
